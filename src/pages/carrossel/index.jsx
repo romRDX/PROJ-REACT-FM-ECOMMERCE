1d6a71d5fa891da6b451fbe5197c73d9df6bd20b
Rômullo S.A.S. <romullo_sas@outlook.com>
2024-04-18 21:06:58 -0300

    PROJ-REACT-010 | Add carousel to home page

---
+++ src/pages/carrossel/index.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import "./carrossel.css";
+
+const Carrossel = () => {
+
+    const [currentPosition, setCurrentPosition] = useState(0);
+
+    const classArray = [
+        'carrossel__img-holder--first',
+        'carrossel__img-holder--second',
+        'carrossel__img-holder--third'
+    ];
+
+    // https://www.cea.com.br/
+
+    // useEffect(() => {
+    //     const slideInterval = setInterval(() => {
+    //         setCurrentPosition((prev) => {
+    //             if(prev < classArray.length -1){
+    //                 return prev +1;
+    //             } else return 0;
+    //         })
+    //     }, 5000);
+
+    //     return () => {
+    //         clearInterval(slideInterval);
+    //     }
+    // }, [])
+
+    const forwardArrow = () => {
+        setCurrentPosition((prev) => {
+            if(prev < classArray.length -1){
+                return prev +1;
+            } else return prev;
+        })
+    }
+
+    const backwardArrow = () => {
+        setCurrentPosition((prev) => {
+            if(prev > 0){
+                return prev -1;
+            } else return prev;
+        })
+    }
+
+    return (
+        <div className='carrossel__container'>
+            <div
+                className='carrossel__arrow carrossel__arrow--left'
+                onClick={backwardArrow}
+            >
+                {'<'}
+            </div>
+
+            <div className='carrossel__slider'>
+                <div
+                    className={
+                        `carrossel__img-holder ${classArray[currentPosition]}`
+                    }
+                >
+                    <img className='carrossel__img carrossel__img--first' src='https://picsum.photos/id/1/1200/400' />
+                    <img className='carrossel__img carrossel__img--second' src='https://picsum.photos/id/2/1200/400' />
+                    <img className='carrossel__img carrossel__img--third' src='https://picsum.photos/id/3/1200/400' />
+                </div>
+            </div>
+
+            <div
+                className='carrossel__arrow carrossel__arrow--right'
+                onClick={forwardArrow}
+            >
+                {'>'}
+            </div>
+
+            <div className='carrossel__position-container'>
+                <div className='carrossel__position-item' />
+                <div className='carrossel__position-item' />
+                <div className='carrossel__position-item' />
+            </div>
+        </div>
+    )
+}
+
+export default Carrossel;
