455abdeca70458927ea64e85c27a99f38aa2e5cc
Rômullo S.A.S. <romullo_sas@outlook.com>
2024-04-25 20:21:56 -0300

    PROJ-REACT-013 | Added product item component to products page

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./products.css";
+import productList from '../../data/productList';
+import ProductItem from '../../components/productItem';
 
 const Products = () => {
 
@@ -8,7 +10,7 @@ const Products = () => {
             <h1 className='products__title'>Products List</h1>
 
             <div className='products__content'>
-                <side className='products__filters'>
+                <aside className='products__filters'>
                     <p className='products__filters__title'>Tamanhos</p>
                     <ul className='products__filters__list'>
                         <li className='products__filters__item'>P</li>
@@ -24,30 +26,16 @@ const Products = () => {
                         <li className='products__filters__item'>verde</li>
                         <li className='products__filters__item'>amarelo</li>
                     </ul>
-                </side>
+                </aside>
 
                 <main className='products__products-list'>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
-                    <li className='products__products-list__item'>item 1</li>
-                    <li className='products__products-list__item'>item 2</li>
-                    <li className='products__products-list__item'>item 3</li>
+                    <ul className='products-list'>
+                        {
+                            productList.map((item) => (
+                                <ProductItem data={item} />
+                            ))
+                        }
+                    </ul>
                 </main>
             </div>
         </div>
